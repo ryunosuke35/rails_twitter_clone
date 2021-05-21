3a61eea8ca631b1950349dcf7181665f7c8c9b23
ryunosuke35 <6bev2103@gmail.com>
2021-05-21 16:04:07 +0900

    バリデーションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "ツイートしました！"
+    else
+      render :new
+    end
   end
   private
   def post_params
